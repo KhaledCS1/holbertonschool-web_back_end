3a9fa9cb6c8b1c043e5a6f7146c090d566ea2f4e
Khaled <Khaled.c003+1@gmail.com>
2025-07-28 23:56:03 +0300

    Create 4-define_variables.py

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This module defines several variables with explicit
+type annotations to demonstrate basic usage of typing in Python.
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
