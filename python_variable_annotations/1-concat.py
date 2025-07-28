139682b014984757639f083bda046b237bcd9596
Khaled <Khaled.c003+1@gmail.com>
2025-07-28 23:50:47 +0300

    Create 1-concat.py

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+This module provides basic mathematical operations such as
+addition using type annotations.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Adds two string-point strings and returns the result.
+
+    Parameters:
+    str1 (str): The first number.
+    str2 (str): The second number.
+
+    Returns:
+    str: The concat of str1 and str2.
+    """
+    return str1 + str2
