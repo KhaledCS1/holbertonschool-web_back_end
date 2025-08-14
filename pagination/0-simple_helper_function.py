d5bfb1056e24bd47472f3561dff2f2f0bd29d8ee
Khaled <Khaled.c003+1@gmail.com>
2025-08-14 11:03:54 +0300

    Update 0-simple_helper_function.py

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -5,6 +5,6 @@ def index_range(page, page_size):
   """
   Ruturn a tuble of start and end indexes for a given page and page size.
   """
-  start = (page - 1) * pageـsize
+  start = (page - 1) * page_size
   end = page * page_size
   return (start, end)
