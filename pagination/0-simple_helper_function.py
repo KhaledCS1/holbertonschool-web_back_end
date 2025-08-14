69fc2766325b9076dbbb37790400e4b703de68a6
Khaled <Khaled.c003+1@gmail.com>
2025-08-14 11:04:13 +0300

    Update 0-simple_helper_function.py

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -3,7 +3,7 @@ from typing import Tuple
 
 def index_range(page, page_size):
   """
-  Ruturn a tuble of start and end indexes for a given page and page size.
+  Return a tuble of start and end indexes for a given page and page size.
   """
   start = (page - 1) * page_size
   end = page * page_size
