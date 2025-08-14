4eb7e5e2f8e7b343d577403c9efca00b6d80de24
KhaledCS1 <10688@holbertonstudents.com>
2025-08-14 11:14:32 +0300

    Fix

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 from typing import Tuple
 
-def index_range(page: int, page_size: int) -> tuple:
-  """
-  Return a tuble of start and end indexes for a given page and page size.
-  """
-  start = (page - 1) * page_size
-  end = page * page_size
-  return (start, end)
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Return a tuple of start and end indexes for a given page and page size.
+    """
+    start = (page - 1) * page_size
+    end = page * page_size
+    return (start, end)
