89de1de02366c0b08b30ab4b1a9195a7dab207f8
KhaledCS1 <10688@holbertonstudents.com>
2025-09-07 05:55:08 +0300

    Added Tasks

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,15 +1,14 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.setEncoding('utf8');
-
-let input = '';
-
-process.stdin.on('data', (data) => {
-  input += data;
+process.stdin.on('readable', () => {
+  const input = process.stdin.read();
+  if (input !== null) {
+    process.stdout.write(`Your name is: ${input}`);
+    process.stdout.write('This important software is now closing\n');
+    process.exit(0);
+  }
 });
 
 process.stdin.on('end', () => {
-  const name = input.trim();
-  process.stdout.write(`Your name is: ${name}\n`);
   process.stdout.write('This important software is now closing\n');
 });
