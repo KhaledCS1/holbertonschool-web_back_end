6848f1e7eb3af3adced9c2a3df08ff17c9ce538f
KhaledCS1 <10688@holbertonstudents.com>
2025-09-07 06:17:13 +0300

    Added Tasks

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -4,8 +4,6 @@ process.stdin.on('readable', () => {
   const input = process.stdin.read();
   if (input !== null) {
     process.stdout.write(`Your name is: ${input}`);
-    process.stdout.write('This important software is now closing\n');
-    process.exit(0);
   }
 });
 
