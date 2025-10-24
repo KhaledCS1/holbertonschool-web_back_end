0b156631cd51825ae5efaedc9ed450ec9506bd2f
Khaled <Khaled.c003+1@gmail.com>
2025-10-24 09:37:40 +0300

    a

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -26,4 +26,3 @@ async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     """
     delays = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
     return sorted(delays)
-  
